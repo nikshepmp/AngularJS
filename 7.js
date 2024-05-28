8a2b7a92d9d1b61ccd9458b92437940fffd13657
Nikshep M P <92396675+nikshepmp@users.noreply.github.com>
2024-05-28 20:39:56 +0530

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,34 @@
+var app=angular.module('userApp',[])
+app.controller('userController',function($scope) 
+{
+    $scope.users = [
+        { name: 'Abhishek', email: 'abhishekab7781@gmail.com'},
+        { name: 'thushar', email: 'urshithaCherugoli@gmail.com'}
+    ];
+
+    $scope.newUser = {};
+
+    $scope.createUser = function() 
+    {
+        $scope.users.push($scope.newUser);
+        $scope.newUser ={};
+    };
+
+
+    $scope.editUser = function(user) 
+    {
+        $scope.editedUser = user;
+    };
+
+
+    $scope.updateUser = function() 
+    {
+        $scope.editedUser = {};
+    };
+
+
+    $scope.deleteUser = function(user) 
+    {
+        $scope.users.splice($scope.users.indexOf(user),1);
+    };
+});
